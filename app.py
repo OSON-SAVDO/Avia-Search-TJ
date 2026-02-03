cbf7746092a80a441e1c5b561a3c364a4a52ddc2
OSON-SAVDO <hadiatulloabdulhajrov@gmail.com>
2026-02-03 12:18:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# САТРҲОИ 8 ва 9: МАЪЛУМОТИ API-И ХУДРО ИНҶО ГУЗОРЕД
-TOKEN = "<script data-noptimize="1" data-cfasync="false" data-wpfc-render="false">
-  (function () {
-      var script = document.createElement("script");
-      script.async = 1;
-      script.src = 'https://emrldco.com/NDk1MDAx.js?t=495001';
-      document.head.appendChild(script);
-  })();
-</script>"  # Аз Travelpayouts гиред
-MARKER = ""    # Аз Travelpayouts гиред
+# ↓↓↓ ДАР ИН ҶО ГУЗОР (АПИ ТОКЕН ВА МАРКЕРРО) ↓↓↓
+TOKEN = "ИНҶО_API_TOKEN_РО_ГУЗОР" # Сатри 8
+MARKER = "ИНҶО_МАРКЕР_РО_ГУЗОР" # Сатри 9
+# ↑↑↑ ТО ИН ҶО ↑↑↑
 
-# Луғати кодҳои шаҳрҳо (IATA)
 CITY_CODES = {
     "Душанбе": "DYU", "Хуҷанд": "LBD", "Москва": "MOW", 
     "Санкт-Петербург": "LED", "Истанбул": "IST", "Дубай": "DXB"
 }
 
-def get_real_flights(origin, destination, date=None):
+def get_flights(origin, destination, date=None):
     url = "https://api.travelpayouts.com/aviasales/v3/prices_for_dates"
     params = {
         "origin": origin, "destination": destination,
@@ -44,11 +37,10 @@ def search():
     to_city = request.form.get('to', 'Москва')
     date = request.form.get('departure_date')
     
-    # Табдил додани номи шаҳр ба код (масалан: Душанбе -> DYU)
     origin = CITY_CODES.get(from_city, "DYU")
     dest = CITY_CODES.get(to_city, "MOW")
     
-    flights = get_real_flights(origin, dest, date)
+    flights = get_flights(origin, dest, date)
     return render_template('index.html', flights=flights, marker=MARKER)
 
 if __name__ == '__main__':
