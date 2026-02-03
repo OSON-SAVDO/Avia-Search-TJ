35c66ad4350080a206c036f5b6d682d5d5290b84
OSON-SAVDO <hadiatulloabdulhajrov@gmail.com>
2026-02-03 20:40:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,44 +3,48 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# ТОКЕН-и худро аз Travelpayouts инҷо гузор
 TOKEN = "71876b59812fee6e1539f9365e6a12dd" 
 MARKER = "701004"
 
+# Функсия барои табдили номи шаҳр ба код (Тюмень -> TJM)
+def get_city_code(city_name):
+    url = f"https://autocomplete.travelpayouts.com/places2?term={city_name}&locale=ru&types[]=city"
+    try:
+        res = requests.get(url).json()
+        return res[0]['code'] if res else city_name.upper()
+    except:
+        return city_name.upper()
+
 @app.route('/')
 def index():
-    # Қадами 1: Саҳифаи асосӣ (Акси 5899)
     return render_template('index.html')
 
 @app.route('/search', methods=['POST'])
 def search():
-    # Қадами 2: Рӯйхати чиптаҳо (Акси 5922)
-    from_city = request.form.get('from', '').upper()
-    to_city = request.form.get('to', '').upper()
+    from_city_name = request.form.get('from')
+    to_city_name = request.form.get('to')
+    
+    origin = get_city_code(from_city_name)
+    destination = get_city_code(to_city_name)
     
     url = "https://api.travelpayouts.com/aviasales/v3/prices_for_dates"
     params = {
-        "origin": from_city,
-        "destination": to_city,
+        "origin": origin,
+        "destination": destination,
         "currency": "rub",
         "token": TOKEN,
         "sorting": "price",
-        "limit": 10
+        "limit": 15
     }
     
-    try:
-        response = requests.get(url, params=params).json()
-        flights = response.get('data', [])
-    except:
-        flights = []
-        
-    return render_template('results.html', flights=flights, from_n=from_city, to_n=to_city)
+    res = requests.get(url, params=params).json()
+    flights = res.get('data', [])
+    
+    return render_template('results.html', flights=flights, from_n=from_city_name, to_n=to_city_name)
 
 @app.route('/details')
 def details():
-    # Қадами 3: Тафсилот (Акси 5920)
-    data = request.args
-    return render_template('details.html', f=data, marker=MARKER)
+    return render_template('details.html', f=request.args, marker=MARKER)
 
 if __name__ == '__main__':
     app.run(debug=True)
